cc38ef82ed80aeb95707387e3a61ffc158d26efc
lactose342 <85597669+lactose342@users.noreply.github.com>
2026-04-22 00:35:09 +0900

    Add Vite configuration with index generator plugin

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { generateIndex } from './generate-index.mjs';
+
+export default {
+  plugins: [
+    {
+      name: 'index-generator',
+      buildStart() {
+        generateIndex(); // 起動時に1回生成
+      },
+      configureServer(server) {
+        // ファイルの追加・削除を監視
+        server.watcher.on('add',    () => generateIndex());
+        server.watcher.on('unlink', () => generateIndex());
+      },
+    },
+  ],
+};
